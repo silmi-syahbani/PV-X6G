2e0649ed1a66721568fe9c452a4bc878be1d6164
Hype <Hype@AMENTI>
2026-04-25 21:20:00 +0700

    first commit

---
+++ src/kasir/koneksi.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kasir;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Hype
+ */
+public class koneksi {
+       private Connection koneksi;
+    public Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("berhasil konek");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("gagal koneksi" + ex);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/project1/kasir";
+
+        try {
+            koneksi = DriverManager.getConnection(url, "root", "");
+            System.out.println("berhasil koneksi database");
+        } catch (SQLException ex) {
+            System.out.println("gagal koneksi database" + ex);
+        }
+
+        return koneksi;
+    }
+}
